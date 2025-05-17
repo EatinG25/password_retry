dab6e0c7128eab7183e03821eed048496c5b68ed
Emily Gong <love555.1011@gmail.com>
2025-05-17 13:11:45 +0800

    pro finished commit

--- password_retry_2.py
+++ password_retry_2.py
@@ -1,12 +1,12 @@
 password = 'a123456'
 chance = 3
-while True:
+while chance > 0:
+	chance = chance - 1
 	inpass = input('Please input passwaord:')
 	if inpass == password:
 		print('success !')
 		break
 	else:
-		chance = chance - 1
 		if chance == 0:
 			print('error ! you have no chance.')
 			break
